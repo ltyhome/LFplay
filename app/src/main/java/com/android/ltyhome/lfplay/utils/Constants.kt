e4c47ae2cbb9b547f23fba79d935807eccef435a
ltyhome <ltyhome@yahoo.com.hk>
2018-06-15 13:51:49 +0800

    a

---
+++ app/src/main/java/com/android/ltyhome/lfplay/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.android.ltyhome.lfplay.utils
+
+/**
+ *创建日期：18-6-15 on 下午1:44
+ *描述：
+ *作者：ltyhome
+ *邮箱：ltyhome@yahoo.com.hk
+ */
+object Constants {
+    val AUTH_URL = "https://ltyhome.us1a.cloud.realm.io/auth"
+}
